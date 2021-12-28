d3a4ced6092720b6bb1fd6c9f4a96fef74c815c3
LxaNce <96795383+LxaNce-Hacker@users.noreply.github.com>
2021-12-29 04:38:30 +0530

    Create voice.py

---
+++ voice.py
@@ -0,0 +1,18 @@
+# Import the Gtts module for text
+# to speech conversion
+#LxaNce🤴👸
+from gtts import gTTS
+
+# import Os module to start the audio file
+import os
+
+# Language we want to use
+language = 'en'
+
+myobj = gTTS(text=a, lang=language, slow=False)
+myobj.save("output.mp3")
+
+# Play the converted file
+os.system("mpv output.mp3")
+
+#We use can also use 'play' in place of mpv.
